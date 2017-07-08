4d204fb1051323eff7e755fff1a3c5a28a74f140
puze8681 <rsef0601@naver.com>
2017-07-09 05:38:10 +0900

    login function

--- app/src/main/java/com/example/parktaejun/nosmoking/LoginActivity.java
+++ app/src/main/java/com/example/parktaejun/nosmoking/LoginActivity.java
@@ -1,13 +1,35 @@
 package com.example.parktaejun.nosmoking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.example.parktaejun.nosmoking.Font.Font;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_regist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Font.setGlobalFont(this, getWindow().getDecorView(), 'r');
         setContentView(R.layout.activity_login);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btn_regist = (Button)findViewById(R.id.register);
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
